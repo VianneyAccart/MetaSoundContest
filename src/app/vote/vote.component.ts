4944a0157b5d93df756ca3a9ad53f4c597900bd1
Vianney Accart <vianneyaccart@gmail.com>
2021-11-25 22:34:18 +0100

    add progress bar in vote page

--- src/app/vote/vote.component.ts
+++ src/app/vote/vote.component.ts
@@ -7,9 +7,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoteComponent implements OnInit {
 
-  constructor(){}
+  progressCount: number;
+  progressBar: number;
+
+  constructor(){
+    this.progressCount = 1;
+    this.progressBar = 10;
+  }
 
   ngOnInit(): void {
   }
 
+  // Each time user like/disklike an instrument, progressBar and progressCount increase and fill the bar
+  increaseProgressCountBar(): void {
+    this.progressCount += 1;
+    this.progressBar += 10;
+  }
 }
